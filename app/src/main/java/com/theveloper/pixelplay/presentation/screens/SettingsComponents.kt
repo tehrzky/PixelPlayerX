05fce4ce41fc2e764509ec9e842b01dba462116c
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-04 19:22:30 +0800

    Add alpha modifier import to SettingsComponents.kt

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/SettingsComponents.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/SettingsComponents.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.draw.alpha
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
