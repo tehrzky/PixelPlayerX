be8c19ad32a757f0db25580150fe34d37abbc5c1
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-05 13:36:36 +0800

    Add items import to AudioFxScreen.kt

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/AudioFxScreen.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/AudioFxScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Extension
 import androidx.compose.material3.Card
