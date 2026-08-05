0b931e8cb0930da8007135ff9ea4489bdff7c63f
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-05 12:12:36 +0800

    Update PluginManagerScreen.kt

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/PluginManagerScreen.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/PluginManagerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
