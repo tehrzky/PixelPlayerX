5f399155588e60a24f3845cbab79de535d591956
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-08 18:18:54 +0800

    Add fillMaxSize import to PluginManagerScreen.kt

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/PluginManagerScreen.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/PluginManagerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
