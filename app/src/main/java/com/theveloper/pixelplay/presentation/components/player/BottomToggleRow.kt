bf7573f8edb1cee8fc8a78f0ccfe918e49cc75dc
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-23 16:24:53 +0800

    Add themed icon types and content descriptions

--- app/src/main/java/com/theveloper/pixelplay/presentation/components/player/BottomToggleRow.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/components/player/BottomToggleRow.kt
@@ -84,13 +84,22 @@ fun BottomToggleRow(
                 inactiveContentColor = inactiveContentColor,
                 onClick = onShuffleToggle,
                 iconId = R.drawable.rounded_shuffle_24,
-                contentDesc = "Shuffle"
+                contentDesc = "Shuffle",
+                themedIconType = if (isShuffleEnabled)
+                    com.theveloper.pixelplay.presentation.components.ThemedIconType.SHUFFLE_ON
+                else
+                    com.theveloper.pixelplay.presentation.components.ThemedIconType.SHUFFLE_OFF
             )
             val repeatActive = repeatMode != Player.REPEAT_MODE_OFF
             val repeatIcon = when (repeatMode) {
                 Player.REPEAT_MODE_ONE -> R.drawable.rounded_repeat_one_24
                 Player.REPEAT_MODE_ALL -> R.drawable.rounded_repeat_24
                 else -> R.drawable.rounded_repeat_24
+            }
+            val repeatThemedType = when (repeatMode) {
+                Player.REPEAT_MODE_ONE -> com.theveloper.pixelplay.presentation.components.ThemedIconType.REPEAT_ONE
+                Player.REPEAT_MODE_ALL -> com.theveloper.pixelplay.presentation.components.ThemedIconType.REPEAT_ALL
+                else -> com.theveloper.pixelplay.presentation.components.ThemedIconType.REPEAT_OFF
             }
             ToggleSegmentButton(
                 modifier = commonModifier,
@@ -102,7 +111,8 @@ fun BottomToggleRow(
                 inactiveContentColor = inactiveContentColor,
                 onClick = onRepeatToggle,
                 iconId = repeatIcon,
-                contentDesc = "Repeat"
+                contentDesc = "Repeat",
+                themedIconType = repeatThemedType
             )
             ToggleSegmentButton(
                 modifier = commonModifier,
@@ -114,7 +124,11 @@ fun BottomToggleRow(
                 inactiveContentColor = inactiveContentColor,
                 onClick = onFavoriteToggle,
                 iconId = if (isFavorite) R.drawable.round_favorite_24 else R.drawable.rounded_favorite_24,
-                contentDesc = "Favorite"
+                contentDesc = "Favorite",
+                themedIconType = if (isFavorite)
+                    com.theveloper.pixelplay.presentation.components.ThemedIconType.FAVORITE_ON
+                else
+                    com.theveloper.pixelplay.presentation.components.ThemedIconType.FAVORITE_OFF
             )
         }
     }
